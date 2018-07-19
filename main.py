9a2572dab90beb6b7bc348a4885becb9467553b7
Sean Kim <sean@Seans-iMac.local>
2018-07-19 15:29:52 +0900

    sesssionAttributes 추가

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ class ClovaServer(BaseHTTPRequestHandler):
             self.set_response(*getattr(self, self.body['request']['intent']['name'])())
             self.do_response()
         except AttributeError:
-            self.set_response('SimpleSpeech', '다시 한 번 말씀해 주세요', False)
+            self.set_response('SimpleSpeech', '다시 한 번 말씀해 주세요', False, None)
             self.do_response()
 
-        del self.speech_body, self.speech_type, self.shouldEndSession
+        del self.speech_body, self.speech_type, self.shouldEndSession, self.sessionAttributes
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
@@ -31,13 +31,14 @@ class ClovaServer(BaseHTTPRequestHandler):
         self.set_header()
         self.do_main()
 
-    def set_response(self, speech_type, speech_body, shouldEndSession):
+    def set_response(self, speech_type, speech_body, shouldEndSession, sessionAttributes):
         self.speech_type = speech_type
         self.speech_body = speech_body
         self.shouldEndSession = shouldEndSession
+        self.sessionAttributes = sessionAttributes
 
     def do_response(self):
-        response_body = {'version': "", "sessionAttributes": None,
+        response_body = {'version': "", "sessionAttributes": self.sessionAttributes,
                          "response":
                              {'outputSpeech':
                                   {"type":
@@ -58,14 +59,14 @@ class ClovaServer(BaseHTTPRequestHandler):
 
     def no_symbol(self, symbol):
         if symbol == None:
-            return 'SimpleSpeech', '해당하는 종목이 없습니다. 코스피 혹은 코스닥시장에 상장 된 종목만 가능합니다. 다시 말씀해 주세요.', False
+            return 'SimpleSpeech', '해당하는 종목이 없습니다. 코스피 혹은 코스닥시장에 상장 된 종목만 가능합니다. 다시 말씀해 주세요.', False, None
         else:
             simmilars = []
             [simmilars.append(key) if symbol in key else None for key in symbol_dict.keys()]
             if len(simmilars) == 0:
-                return 'SimpleSpeech', '해당하는 종목이 없습니다. 코스피 혹은 코스닥시장에 상장 된 종목만 가능합니다. 다시 말씀해 주세요.', False
+                return 'SimpleSpeech', '해당하는 종목이 없습니다. 코스피 혹은 코스닥시장에 상장 된 종목만 가능합니다. 다시 말씀해 주세요.', False, None
             else:
-                return 'SimpleSpeech', symbol + '이 들어가는 종목은 ' +', '.join(simmilars) + '이 있습니다. 이 중 하나를 말씀해 주세요.', False
+                return 'SimpleSpeech', symbol + '이 들어가는 종목은 ' +', '.join(simmilars) + '이 있습니다. 이 중 하나를 말씀해 주세요.', False, None
 
     def recentnews(self):
         # 3문장으로 요약하도록 해 두었음, 결과가 적절하지 않을 시 수정 요망
@@ -84,10 +85,7 @@ class ClovaServer(BaseHTTPRequestHandler):
         # https://developers.naver.com/console/clova/guide/CEK/References/CEK_API.md#CustomExtSpeechInfoObject
         for ll in speech_list:
             speech_text += ll
-        return 'SpeechList', speech_text, True
-
-
-
+        return 'SpeechList', speech_text, True, None
 
 
 def run(server_class=HTTPServer, handler_class=ClovaServer, port=80):
