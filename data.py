538a1a1d4203378bbcc0fed46b317411abcf154b
Sean Kim <sean@Seans-iMac.local>
2018-07-19 22:14:02 +0900

    crawl from mobile

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from urllib.parse import urlparse, parse_qs
 
 class User():
     def __init__(self, user_id):
@@ -11,3 +12,9 @@ class User():
 
     def save_data(self):
         self.data.to_csv('./user_data/' + self.user_id)
+
+def to_mobile_page(link):
+    parsed = urlparse(link)
+    parsed = parse_qs(parsed.query)
+    return 'https://m.stock.naver.com/item/main.nhn#/stocks/{}/news/{}/office/{}'\
+                                    .format(parsed['code'][0], parsed['article_id'][0], parsed['office_id'][0])
